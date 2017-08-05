48c27c873ac30adb8749ee0658e0092034a57951
Daniel Watkins <daniel@daniel-watkins.co.uk>
2017-08-05 16:35:21 -0400

    Only parametrize integration tests in pytest_generate_tests

--- integration_tests/tests.py
+++ integration_tests/tests.py
@@ -56,5 +56,6 @@ def _parse_testcases(filename):
 def pytest_generate_tests(metafunc):
     test_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
     test_cases = _parse_testcases(os.path.join(test_dir, 'tests.ini'))
-    metafunc.parametrize('integration_testcase', test_cases,
-                         ids=lambda testcase: testcase.test_name)
+    if 'integration_testcase' in metafunc.fixturenames:
+        metafunc.parametrize('integration_testcase', test_cases,
+                            ids=lambda testcase: testcase.test_name)
