fdb0e1c1445e0d5f8a920936a18ec4f0040b0ba9
Daniel Watkins <daniel@daniel-watkins.co.uk>
2017-08-12 12:34:17 -0400

    Add .getlist() method to the config parser we use internally

--- jenkins_job_linter/config.py
+++ jenkins_job_linter/config.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 """Handle configuration for jenkins-job-linter."""
 from configparser import ConfigParser
-from typing import Any, Dict
+from typing import Any, Dict, List, cast
 
 from jenkins_job_linter.linters import LINTERS
 
@@ -21,6 +21,22 @@ GLOBAL_CONFIG_DEFAULTS = {
     'disable_linters': [],
     'only_run': None,
 }  # type: Dict[str, Any]
+
+
+class GetListConfigParser(ConfigParser):
+    """A ConfigParser subclass that implements a getlist method."""
+
+    def getlist(self, section: str, option: str) -> List[str]:
+        """Parse an option, splitting it by commas and stripping whitespace."""
+        def commas_to_list(value: str) -> List[str]:
+            if not value:
+                return []
+            return [item.strip() for item in value.split(',')]
+
+        # These type shenanigans can be removed once
+        # https://github.com/python/typeshed/pull/1542 is released
+        ret = self._get_conv(section, option, commas_to_list)  # type: ignore
+        return cast(List[str], ret)
 
 
 def _get_default_linter_configs() -> Dict[str, Dict[str, Any]]:
@@ -35,7 +51,7 @@ def _filter_config(config: ConfigParser) -> ConfigParser:
     This creates a new ConfigParser and removes sections from that, so the one
     passed in remains unmodified.
     """
-    filtered_config = ConfigParser(allow_no_value=True)
+    filtered_config = GetListConfigParser(allow_no_value=True)
     filtered_config.read_dict({'job_linter': GLOBAL_CONFIG_DEFAULTS})
     filtered_config.read_dict(_get_default_linter_configs())
     filtered_config.read_dict(config)
